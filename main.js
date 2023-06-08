f89696005abe73a5a2a9e4fc47cecd877027ceb2
YashiAhana <108013639+YashiAhana@users.noreply.github.com>
2023-06-08 16:42:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump=loadSound("jump.wav")
+	mario_kick=loadSound("kick.wav")
+	mario_coin=loadSound("coin.wav")
+	mario_die=loadSound("mariodie.wav")
+	mario_gameover=loadSound("gameover.wav")
+
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	canvas.parent(document.getElementById("canvas"))
+
+	video=createCapture(VIDEO)
+	video.size(800,400)
+	video.parent(document.getElementById("game_console"))
+
+	posenet=ml5.poseNet(video,modelLoaded)
+	posenet.on("pose",gotPoses)
+
+}
+
+function modelLoaded(){
+	console.log("Model is loaded")
+}
+
+function gotPoses(results){
+if(results.length>0){
+	console.log(results)
+	noseX=results[0].pose.nose.x
+	noseY=results[0].pose.nose.y
+}
+
+}
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
